6b18c5794918bb525ecef0016abd17e6f285ce13
Giovanni <giovagalassi8@gmail.com>
2026-04-14 10:49:41 +0200

    Update AMAS_matrix_stats.sh

--- AMAS_matrix_stats.sh
+++ AMAS_matrix_stats.sh
@@ -13,6 +13,9 @@ OUTDIR="matrix_stats"
 # Create output directory if it does not exist.
 mkdir -p "$OUTDIR"
 
+# Create the output file that will contains all the summary statistics (change name if necessary).
+FINAL_SUMMARY="$OUTDIR/all_matrix_stats.txt"
+
 # Recursively find all concatenated_*.out files.
 find . -type f -name "concatenated_*.out" | while read -r file; do
     # Extract filename without path
@@ -40,3 +43,11 @@ find . -type f -name "concatenated_*.out" | while read -r file; do
 
     echo "Processed: $basefile → summary_${core_name}.txt"
 done
+
+# Combine all summaries into a single file while keeping the header.
+head -n 1 "$(ls "$OUTDIR"/summary_*.txt | head -1)" > "$FINAL_SUMMARY"
+for f in "$OUTDIR"/summary_*.txt; do
+    tail -n +2 "$f" >> "$FINAL_SUMMARY"
+done
+
+echo "Combined summary: $FINAL_SUMMARY"
